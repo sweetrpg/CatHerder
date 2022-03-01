2863be597fc64224570813394eda2583810b66fb
Paul Schifferer <paul@schifferers.net>
2022-03-01 11:42:56 -0800

    Set occlusion shape on mouse trap.

--- src/main/java/com/sweetrpg/catherder/common/block/MouseTrapBlock.java
+++ src/main/java/com/sweetrpg/catherder/common/block/MouseTrapBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
@@ -20,11 +21,15 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class MouseTrapBlock extends Block {
 
     public static final BooleanProperty SPRUNG = BlockStateProperties.TRIGGERED;
+    protected static final VoxelShape SHAPE = Block.box(1.0D, 0.0D, 0.0D, 15.0D, 2.0D, 16.0D);
+    protected static final VoxelShape SHAPE_COLLISION = Block.box(1.0D, 0.0D, 0.0D, 15.0D, 2.0D, 16.0D);
 
     public MouseTrapBlock() {
         super(Block.Properties.of(Material.WOOD).strength(3.0F, 5.0F).sound(SoundType.WOOD));
@@ -32,6 +37,21 @@ public class MouseTrapBlock extends Block {
 
     public boolean hasDynamicShape() {
         return true;
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext selectionContext) {
+        return SHAPE_COLLISION;
     }
 
     @Nullable
