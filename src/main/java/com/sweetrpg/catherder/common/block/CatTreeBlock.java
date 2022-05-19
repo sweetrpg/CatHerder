c59283df0b8bae5404a430001faac2156468a4e9
Paul Schifferer <paul@schifferers.net>
2022-05-19 08:01:40 -0700

    Adjust occlusion height on cat tree, so it doesn't cause adjacent blocks to disappear.

--- src/main/java/com/sweetrpg/catherder/common/block/CatTreeBlock.java
+++ src/main/java/com/sweetrpg/catherder/common/block/CatTreeBlock.java
@@ -58,7 +58,7 @@ public class CatTreeBlock extends BaseEntityBlock {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 29.0D, 16.0D);
-    protected static final VoxelShape SHAPE_OCCLUSION = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 29.0D, 16.0D);
+    protected static final VoxelShape SHAPE_OCCLUSION = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
     protected static final VoxelShape SHAPE_COLLISION = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 29.0D, 16.0D);
 
     public CatTreeBlock() {
