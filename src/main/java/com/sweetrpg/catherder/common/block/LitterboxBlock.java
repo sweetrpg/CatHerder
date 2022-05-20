63d13a1e8625e466895c5391a09090140e698f2e
Paul Schifferer <paul@schifferers.net>
2022-05-19 18:21:11 -0700

    Fix casing of litterbox classes.

--- src/main/java/com/sweetrpg/catherder/common/block/LitterboxBlock.java
+++ src/main/java/com/sweetrpg/catherder/common/block/LitterboxBlock.java
@@ -26,13 +26,13 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class LitterBoxBlock extends Block {
+public class LitterboxBlock extends Block {
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
     protected static final VoxelShape SHAPE_COLLISION = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D);
 
-    public LitterBoxBlock() {
+    public LitterboxBlock() {
         super(Block.Properties.of(Material.METAL).strength(3.0F, 5.0F).sound(SoundType.METAL));
     }
 
