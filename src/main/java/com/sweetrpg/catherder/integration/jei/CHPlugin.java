bd3bb3b8b6bd91cf241a5538c27c96860d3a846b
Paul Schifferer <paul@schifferers.net>
2022-06-15 21:49:43 -0700

    Cleanup

--- src/main/java/com/sweetrpg/catherder/integration/jei/CHPlugin.java
+++ src/main/java/com/sweetrpg/catherder/integration/jei/CHPlugin.java
@@ -18,29 +18,28 @@ import java.util.stream.Collectors;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @SuppressWarnings("unused")
-public class CHPlugin implements IModPlugin
-{
-	private static final ResourceLocation ID = new ResourceLocation(Constants.MOD_ID, "ch_plugin");
-	private static final Minecraft MC = Minecraft.getInstance();
+public class CHPlugin implements IModPlugin {
+    private static final ResourceLocation ID = new ResourceLocation(Constants.MOD_ID, "ch_plugin");
+    private static final Minecraft MC = Minecraft.getInstance();
 
-	private static List<Recipe<?>> findRecipesByType(RecipeType<?> type) {
-		return MC.level
-				.getRecipeManager()
-				.getRecipes()
-				.stream()
-				.filter(r -> r.getType() == type)
-				.collect(Collectors.toList());
-	}
+    private static List<Recipe<?>> findRecipesByType(RecipeType<?> type) {
+        return MC.level
+                       .getRecipeManager()
+                       .getRecipes()
+                       .stream()
+                       .filter(r -> r.getType() == type)
+                       .collect(Collectors.toList());
+    }
 
-	@Override
-	public void registerCategories(IRecipeCategoryRegistration registry) {
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registry) {
 //		registry.addRecipeCategories(new CookingRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
 //		registry.addRecipeCategories(new CuttingRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
 //		registry.addRecipeCategories(new DecompositionRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
-	}
+    }
 
-	@Override
-	public void registerRecipes(IRecipeRegistration registration) {
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
 //		registration.addRecipes(findRecipesByType(CookingPotRecipe.TYPE), CookingRecipeCategory.UID);
 //		registration.addRecipes(findRecipesByType(CuttingBoardRecipe.TYPE), CuttingRecipeCategory.UID);
 //		registration.addRecipes(ImmutableList.of(new DecompositionDummy()), DecompositionRecipeCategory.UID);
@@ -52,29 +51,29 @@ public class CHPlugin implements IModPlugin
 //		registration.addIngredientInfo(new ItemStack(ModItems.DIAMOND_KNIFE.get()), VanillaTypes.ITEM, TextUtils.getTranslation("jei.info.knife"));
 //		registration.addIngredientInfo(new ItemStack(ModItems.NETHERITE_KNIFE.get()), VanillaTypes.ITEM, TextUtils.getTranslation("jei.info.knife"));
 //		registration.addIngredientInfo(new ItemStack(ModItems.GOLDEN_KNIFE.get()), VanillaTypes.ITEM, TextUtils.getTranslation("jei.info.knife"));
-	}
+    }
 
-	@Override
-	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
 //		registration.addRecipeCatalyst(new ItemStack(ModItems.COOKING_POT.get()), CookingRecipeCategory.UID);
 //		registration.addRecipeCatalyst(new ItemStack(ModItems.CUTTING_BOARD.get()), CuttingRecipeCategory.UID);
 //		registration.addRecipeCatalyst(new ItemStack(ModItems.STOVE.get()), VanillaRecipeCategoryUid.CAMPFIRE);
 //		registration.addRecipeCatalyst(new ItemStack(ModItems.SKILLET.get()), VanillaRecipeCategoryUid.CAMPFIRE);
 //		registration.addRecipeCatalyst(new ItemStack(ModBlocks.ORGANIC_COMPOST.get()), DecompositionRecipeCategory.UID);
-	}
+    }
 
-	@Override
-	public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
 //		registration.addRecipeClickArea(CookingPotScreen.class, 89, 25, 24, 17, CookingRecipeCategory.UID);
-	}
+    }
 
-	@Override
-	public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
 //		registration.addRecipeTransferHandler(CookingPotContainer.class, CookingRecipeCategory.UID, 0, 6, 9, 36);
-	}
+    }
 
-	@Override
-	public ResourceLocation getPluginUid() {
-		return ID;
-	}
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
 }
