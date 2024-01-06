1c942d0faf02d3022f550daf6d2b0d644ca81d2a
Paul Schifferer <paul@schifferers.net>
2024-01-06 10:22:02 -0800

    CI build change

--- src/main/java/com/sweetrpg/catherder/common/block/entity/PetDoorBlockEntity.java
+++ src/main/java/com/sweetrpg/catherder/common/block/entity/PetDoorBlockEntity.java
@@ -8,8 +8,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.model.data.IModelData;
-import net.minecraftforge.client.model.data.ModelDataMap;
 import net.minecraftforge.client.model.data.ModelProperty;
 
 public class PetDoorBlockEntity extends PlacedBlockEntity {
