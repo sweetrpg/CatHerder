c052ffbdfd613b0b97f27f35a2a66e32da3a0c4a
Paul Schifferer <paul@schifferers.net>
2024-01-07 09:49:53 -0800

    Fix overridden vanilla loot tables.
    Signed-off-by: Paul Schifferer <paul@schifferers.net>

---
+++ src/main/java/com/sweetrpg/catherder/common/loot/CatGutLootModifier.java
@@ -0,0 +1,22 @@
+//package com.sweetrpg.catherder.common.loot;
+//
+//import net.minecraft.world.item.ItemStack;
+//import net.minecraft.world.level.storage.loot.LootContext;
+//import net.minecraftforge.common.loot.LootModifier;
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.util.List;
+//
+//public class CatGutLootModifier extends LootModifier {
+//
+//    public CatGutLootModifier(ILootCondition[] conditionsIn) {
+//        super(conditionsIn);
+//        // Store the rest of the parameters
+//    }
+//
+//    @NotNull
+//    @Override
+//    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+//
+//    }
+//}
